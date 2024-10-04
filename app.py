64089eba64aabf7858b64c776bef23d9899fd022
Noguchi <you@example.com>
2024-10-04 10:50:49 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  3 10:21:35 2024
+
+@author: USTPC144
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/index")
+def home():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/access")
+def access():
+    return render_template('access.html')
+
+@app.route("/menu")
+def menu():
+    return render_template('menu.html')
+
+if __name__ == "__main__":
+    app.run()
