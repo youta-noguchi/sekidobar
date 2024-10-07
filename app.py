3700e822f6d1b3bc7c00b82d661ef520baa13559
Noguchi <you@example.com>
2024-10-07 18:45:16 +0900

    fourth commit

--- app.py
+++ app.py
@@ -5,9 +5,20 @@ Created on Thu Oct  3 10:21:35 2024
 @author: USTPC144
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+
+#メール設定
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587                             # TLSは587、SSLなら465
+app.config['MAIL_USERNAME'] = 'shimaoosei@gmail.com'
+app.config['MAIL_PASSWORD'] = 'xbzbotcdcvqosxpu'        # GmailのApp用のmパスワード設定をしておく必要あり
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_DEFAULT_SENDER'] = 'shimaoosei@gmail.com'    # これがあるとsender設定が不要になる
+mail = Mail(app)
 
 # トップページ遷移時の処理
 @app.route("/index")
@@ -35,14 +46,28 @@ def menu():
     return render_template('menu.html', title = title)
 
 # お問い合わせ遷移時の処理
-@app.route("/contact")
+@app.route("/contact", methods=['GET'])
 def contact():
     title = 'お問い合わせ'
     return render_template('contact.html', title = title)
 
-#####テスト#####
+# お問い合わせ送信時の処理
+@app.route("/contact", methods=['POST'] )
+def send_email():
+    title = 'お問い合わせ完了'
 
-#####テスト#####
+#   Form内容受取
+    username    = request.form['data1']
+    hurigana    = request.form['data2']
+    email       = request.form['data3']
+    description = request.form['data4']
+
+#   メール作成
+    msg = Message('お問い合わせ', recipients=['noguchi@ust-solution.co.jp'])
+    msg.body = f"お名前：{username}\nふりがな：{hurigana}\nメールアドレス：{email}\n問い合わせ内容：\n{description}"
+    mail.send(msg)
+    
+    return render_template('contact.html', title = title)
 
 if __name__ == "__main__":
     app.run()
